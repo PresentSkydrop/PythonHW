54859195e8152f42c0ab5a578a25a11c9ba0e8fb
PresentSkydrop <elizabete.veiss@gmail.com>
2023-03-05 21:51:14 +0200

    assigning values to variables assignment

---
+++ presentation2_1.py
@@ -0,0 +1,14 @@
+# Presentation 2—23/02/2023
+
+
+a = "Elizabete" # variable a w/ YOUR NAME
+b = "Hello SheGoesTech" # variable b w/ Hello SheGoesTech
+
+#print each variable seperatley
+print(a)
+print(b)
+
+#print out both variables together
+print(a + " "+ b)
+print(b[0:5] + " "+ a + ", "+  b[6:])
+print(b, a)
