1c964747028ec59a81df475f3c5b2fc528e0843d
PresentSkydrop <elizabete.veiss@gmail.com>
2023-03-05 21:31:29 +0200

    first assignment, statuss: added

---
+++ presentation1.py
@@ -0,0 +1,6 @@
+#Presentation 1— 21/02/2023
+# Create a programm that writes out any text 
+
+print("This programm writes out any text.")
+anyText = input("Just so it could actually be any text, how about you enter some and we'll write it out? Write! ")
+print("You have entered such text: \n" + anyText)
